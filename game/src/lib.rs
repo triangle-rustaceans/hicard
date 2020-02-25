e8ea0e2017bc9326f66fe886bf0fe7f232c2c58f
Eric Harding <eric.harding@bluetradesys.com>
2020-02-24 20:33:10 -0500

    place for game logic

--- game/src/lib.rs
+++ game/src/lib.rs
@@ -1,24 +1,45 @@
 
-use std::cmp::{Ord,PartialOrd, Eq, PartialEq};
+// use std::cmp::{Ord,PartialOrd, Eq, PartialEq};
+use std::collections::HashMap;
+use uuid::Uuid;
+use deckofcards::{Card,Deck};
 
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
-pub enum Suit {
-    Spade = 4,
-    Heart = 3,
-    Diamond = 2,
-    Club = 1
+struct Player {
+    name : String,
+    id : Uuid,
+    card : Option<Card>
 }
 
+impl Player {
+    fn new(name : &str) -> Player {
+        Player { 
+            name: name.to_owned(), 
+            id: Uuid::new_v4(),
+            card : None
+        }
+    }
+}
+
+pub struct Game {
+    deck : Deck,
+    players : HashMap<String,Player>
+}
+
+impl Game {
+    fn new() -> Game { 
+        Game { deck : Deck::new(), players : HashMap::new() }
+    }
+    fn join(&mut self, name : &str) -> &Player {
+
+    }
+}
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn suit_order() {
-        let suits = [Suit::Spade, Suit::Heart, Suit::Diamond, Suit::Club];
-        for s in 0..3 {
-            assert_eq!(true, suits[s] > suits[s+1]);
-        }
+    fn play_game() {
+        let game = Game::new();
     }
 }
