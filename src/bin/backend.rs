6348c38f053c84599eb85bb4b098b1c00c1ce3ce
Tom Pusateri <pusateri@bangj.com>
2020-02-24 20:12:17 -0500

    Add more URL endpoints.

--- src/bin/backend.rs
+++ src/bin/backend.rs
@@ -3,11 +3,36 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    // main page
+    let root = warp::path::end()
+        .map(|| format!("Welcome to the HiCard"));
 
-    warp::serve(hello)
+    // poll for turn, get
+    let game_get = warp::path!("game")
+    	.and(warp::get())
+    	.map(|| format!("got game"));
+
+    // join game with post
+    let game_post = warp::path!("game")
+    	.and(warp::post())
+    	.map(|| format!("join game"));
+
+    // join game with post
+    let move_post = warp::path!("move")
+    	.and(warp::post())
+    	.map(|| format!("make a move"));
+
+    // wait for winner result
+    let result = warp::path!("results")
+    	.map(|| format!("show results"));
+
+    let routes = root
+    	.or(game_get)
+    	.or(game_post)
+    	.or(move_post)
+    	.or(result);
+
+    warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
