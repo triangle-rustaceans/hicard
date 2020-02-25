1bcf4e45bc0cad8e3cabd3d6bc210646d67cca21
Tom Pusateri <pusateri@bangj.com>
2020-02-24 21:01:12 -0500

    Broken current state of backend.

--- src/bin/backend.rs
+++ src/bin/backend.rs
@@ -1,5 +1,31 @@
 
 use warp::Filter;
+use warp::http::StatusCode;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use std::convert::Infallible;
+use uuid::Uuid;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct User {
+	user_name: String,
+	user_uuid: Uuid,
+}
+
+fn json_body() -> impl Filter<Extract = (User,), Error = warp::Rejection> + Clone {
+    // When accepting a body, we want a JSON body
+    // (and to reject huge payloads)...
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
+
+pub async fn join_game(create: User) -> Result<impl warp::Reply, Infallible> {
+	// create a new Uuid, and send it back to the user
+
+	let user_uuid = Uuid::new_v4();
+
+    Ok(StatusCode::CREATED)
+}
 
 #[tokio::main]
 async fn main() {
@@ -15,12 +41,13 @@ async fn main() {
     // join game with post
     let game_post = warp::path!("game")
     	.and(warp::post())
-    	.map(|| format!("join game"));
+    	.and(json_body())
+    	.and_then(join_game);
 
     // join game with post
     let move_post = warp::path!("move")
     	.and(warp::post())
-    	.map(|| format!("make a move"));
+    	.map(warp::reply);
 
     // wait for winner result
     let result = warp::path!("results")
